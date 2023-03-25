01e9dc54f542a29489a3624e69736438cc19bf40
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2023-03-25 17:38:24 +0100

    first attempt

---
+++ Organiser.Core/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Organiser.Core.Entities;
+
+namespace Organiser.Core
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<TestDB> TestDBs => Set<TestDB>();
+    }
+}
---
+++ Organiser.Core/Entities/TestDB.cs
@@ -0,0 +1,9 @@
+﻿namespace Organiser.Core.Entities
+{
+    public class TestDB
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public bool vary { get; set; }
+    }
+}
