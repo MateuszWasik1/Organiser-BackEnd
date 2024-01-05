d2ac428095fcc6b3bea5944df7d5770b667ea4c3
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2024-01-05 16:29:55 +0100

    UTests StatsController - GetMoneySpendedForCategoryBarChart

--- Organiser.Core/Organiser.UnitTests/Controllers/TestStatsController.cs
+++ Organiser.Core/Organiser.UnitTests/Controllers/TestStatsController.cs
@@ -5,7 +5,6 @@ using Organiser.Cores.Context;
 using Organiser.Cores.Controllers;
 using Organiser.Cores.Entities;
 using Organiser.Cores.Models.Enums;
-using Organiser.Cores.Models.ViewModels;
 
 namespace Organiser.UnitTests.Controllers
 {
@@ -63,6 +62,7 @@ namespace Organiser.UnitTests.Controllers
                 {
                     TID = 1,
                     TGID = new Guid("00dacc1d-7bee-4635-9c4c-9404a4af80dd"),
+                    TCGID = new Guid("f3dacc1d-7bee-4635-9c4c-9404a4af80dd"),
                     TUID = 1,
                     TBudget = 245,
                     TName = "Name1",
@@ -74,6 +74,7 @@ namespace Organiser.UnitTests.Controllers
                 {
                     TID = 2,
                     TGID = new Guid("10dacc1d-7bee-4635-9c4c-9404a4af80dd"),
+                    TCGID = new Guid("f3dacc1d-7bee-4635-9c4c-9404a4af80dd"),
                     TUID = 1,
                     TBudget = 2135,
                     TName = "Name2",
@@ -85,6 +86,7 @@ namespace Organiser.UnitTests.Controllers
                 {
                     TID = 3,
                     TGID = new Guid("20dacc1d-7bee-4635-9c4c-9404a4af80dd"),
+                    TCGID = new Guid("f3dacc1d-7bee-4635-9c4c-9404a4af80dd"),
                     TUID = 1,
                     TBudget = 4455,
                     TName = "Name3",
@@ -102,39 +104,9 @@ namespace Organiser.UnitTests.Controllers
                     CGID = new Guid("f3dacc1d-7bee-4635-9c4c-9404a4af80dd"),
                     CUID = 1,
                     CName = "Nazwa 1",
-                    CBudget = 2050,
+                    CBudget = 24555,
                     CStartDate = new DateTime(2023, 12, 4, 21, 30, 0),
                     CEndDate = new DateTime(2023, 12, 5, 21, 30, 0)
-                },
-                new Categories()
-                {
-                    CID = 2,
-                    CGID = new Guid("f5dacc1d-7bee-4635-9c4c-9404a4af80dd"),
-                    CUID = 1,
-                    CName = "Nazwa 2",
-                    CBudget = 2060,
-                    CStartDate = new DateTime(2023, 12, 1, 21, 30, 0),
-                    CEndDate = new DateTime(2023, 12, 5, 21, 30, 0)
-                },
-                new Categories()
-                {
-                    CID = 3,
-                    CGID = new Guid("f7dacc1d-7bee-4635-9c4c-9404a4af80dd"),
-                    CUID = 1,
-                    CName = "Nazwa 3",
-                    CBudget = 2070,
-                    CStartDate = new DateTime(2023, 12, 1, 21, 30, 0),
-                    CEndDate = new DateTime(2023, 12, 6, 21, 30, 0)
-                },
-                new Categories()
-                {
-                    CID = 4,
-                    CGID = new Guid("f9dacc1d-7bee-4635-9c4c-9404a4af80dd"),
-                    CUID = 44,
-                    CName = "Nazwa 4",
-                    CBudget = 2050,
-                    CStartDate = new DateTime(2023, 12, 1, 21, 30, 0),
-                    CEndDate = new DateTime(2023, 12, 4, 21, 30, 0)
                 },
             };
 
@@ -251,85 +223,83 @@ namespace Organiser.UnitTests.Controllers
             ClassicAssert.AreEqual(2380M, result?.Datasets?.Data[1]);
         }
 
-        //[Test]
-        //public void TestCategoriesController_AddCategory_CategoryExistButErrorIsThrow_ShouldThrowException()
-        //{
-        //    //Arrange
-        //    var controller = new CategoriesController(context.Object, mapper.Object);
+        //GetMoneySpendedForCategoryBarChart
+        [Test]
+        public void TestStatsController_GetMoneySpendedForCategoryBarChart_CGIDIsEmpty_ShouldReturnEmptyDataSets()
+        {
+            //Arrange
+            var controller = new StatsController(context.Object);
 
-        //    var category = new CategoriesViewModel()
-        //    {
-        //        CID = 2,
-        //        CGID = new Guid("99dacc1d-7bee-4635-9c4c-9404a4af80dd"),
-        //    };
+            //Act
+            var result = controller.GetMoneySpendedForCategoryBarChart(new DateTime(2024, 1, 1, 0, 0, 0), new DateTime(2024, 1, 31, 23, 59, 59), Guid.Empty);
 
-        //    //Act
-        //    //Assert
-        //    Assert.Throws<Exception>(() => controller.Save(category));
-        //}
+            //Assert
+            ClassicAssert.AreEqual(null, result?.Labels?.Count);
+            ClassicAssert.AreEqual(null, result?.Datasets?.Data?.Count);
+        }
 
-        //[Test]
-        //public void TestCategoriesController_AddCategory_CategoryExist_ShouldModifyCategory()
-        //{
-        //    //Arrange
-        //    var controller = new CategoriesController(context.Object, mapper.Object);
+        [Test]
+        public void TestStatsController_GetMoneySpendedForCategoryBarChart_CGIDIsNotFound_ShouldThrowException()
+        {
+            //Arrange
+            var controller = new StatsController(context.Object);
 
-        //    var category = new CategoriesViewModel()
-        //    {
-        //        CID = 4,
-        //        CGID = new Guid("f9dacc1d-7bee-4635-9c4c-9404a4af80dd"),
-        //        CUID = 44,
-        //        CName = "Nazwa 5",
-        //        CBudget = 2060,
-        //        CStartDate = new DateTime(2023, 12, 5, 21, 30, 0),
-        //        CEndDate = new DateTime(2023, 12, 12, 21, 30, 0)
-        //    };
+            //Act
+            //Assert
+            Assert.Throws<Exception>(() => controller.GetMoneySpendedForCategoryBarChart(new DateTime(2024, 1, 1, 0, 0, 0), new DateTime(2024, 1, 31, 23, 59, 59), Guid.NewGuid()));
+        }
 
-        //    //Act
-        //    controller.Save(category);
+        [Test]
+        public void TestStatsController_GetMoneySpendedForCategoryBarChart_TakeSavingsOnlyFromJanuary2024_ShouldReturnDataFromSavingsFromJanuary2024()
+        {
+            //Arrange
+            var controller = new StatsController(context.Object);
 
-        //    //Assert
-        //    ClassicAssert.AreEqual(4, categories.Count);
-        //    ClassicAssert.AreEqual("Nazwa 5", categories[3].CName);
-        //    ClassicAssert.AreEqual(2060, categories[3].CBudget);
-        //    ClassicAssert.AreEqual(new DateTime(2023, 12, 5, 21, 30, 0), categories[3].CStartDate);
-        //    ClassicAssert.AreEqual(new DateTime(2023, 12, 12, 21, 30, 0), categories[3].CEndDate);
-        //}
+            //Act
+            var result = controller.GetMoneySpendedForCategoryBarChart(new DateTime(2024, 1, 1, 0, 0, 0), new DateTime(2024, 1, 31, 23, 59, 59), categories[0].CGID);
 
-        ////Delete
-        //[Test]
-        //public void TestCategoriesController_DeleteCategory_CategoryNotFound_ShouldThrowException()
-        //{
-        //    //Arrange
-        //    var controller = new CategoriesController(context.Object, mapper.Object);
+            //Assert
+            ClassicAssert.AreEqual(1, result?.Labels?.Count);
+            ClassicAssert.AreEqual("2024-1", result?.Labels[0]);
 
-        //    //Act
-        //    //Assert
-        //    Assert.Throws<Exception>(() => controller.Delete(Guid.Empty));
-        //}
+            ClassicAssert.AreEqual("Wydane pieniądze na daną kategorię", result?.Datasets.Label);
+            ClassicAssert.AreEqual(2380M, result?.Datasets?.Data[0]);
+        }
 
-        //[Test]
-        //public void TestCategoriesController_DeleteCategory_CategoryHasTasks_ShouldThrowException()
-        //{
-        //    //Arrange
-        //    var controller = new CategoriesController(context.Object, mapper.Object);
+        [Test]
+        public void TestStatsController_GetMoneySpendedForCategoryBarChart_TakeSavingsOnlyFromFirstHalfJanuary2024_ShouldReturnDataFromSavingsOnlyFromFirstHalfOfJanuary2024()
+        {
+            //Arrange
+            var controller = new StatsController(context.Object);
 
-        //    //Act
-        //    //Assert
-        //    Assert.Throws<Exception>(() => controller.Delete(Guid.Empty));
-        //}
+            //Act
+            var result = controller.GetMoneySpendedForCategoryBarChart(new DateTime(2024, 1, 1, 0, 0, 0), new DateTime(2024, 1, 14, 23, 59, 59), categories[0].CGID);
 
-        //[Test]
-        //public void TestCategoriesController_DeleteCategory_CategoryIsFound_ShouldDeleteCategory()
-        //{
-        //    //Arrange
-        //    var controller = new CategoriesController(context.Object, mapper.Object);
+            //Assert
+            ClassicAssert.AreEqual(1, result?.Labels?.Count);
+            ClassicAssert.AreEqual("2024-1", result?.Labels[0]);
 
-        //    //Act
-        //    controller.Delete(categories[2].CGID);
+            ClassicAssert.AreEqual("Wydane pieniądze na daną kategorię", result?.Datasets.Label);
+            ClassicAssert.AreEqual(245M, result?.Datasets?.Data[0]);
+        }
 
-        //    //Assert
-        //    ClassicAssert.AreEqual(3, categories.Count);
-        //}
+        [Test]
+        public void TestStatsController_GetMoneySpendedForCategoryBarChart_TakeSavingsFromDecember2023AndJanuary2024_ShouldReturnDataFromSavingsFromFDecember2023AndJanuary2024()
+        {
+            //Arrange
+            var controller = new StatsController(context.Object);
+
+            //Act
+            var result = controller.GetMoneySpendedForCategoryBarChart(new DateTime(2023, 12, 1, 0, 0, 0), new DateTime(2024, 1, 31, 23, 59, 59), categories[0].CGID);
+
+            //Assert
+            ClassicAssert.AreEqual(2, result?.Labels?.Count);
+            ClassicAssert.AreEqual("2023-12", result?.Labels[0]);
+            ClassicAssert.AreEqual("2024-1", result?.Labels[1]);
+
+            ClassicAssert.AreEqual("Wydane pieniądze na daną kategorię", result?.Datasets.Label);
+            ClassicAssert.AreEqual(4455M, result?.Datasets?.Data[0]);
+            ClassicAssert.AreEqual(2380M, result?.Datasets?.Data[1]);
+        }
     }
 }
