a015f366c7caf1318e5cda832709e03d7bdaf70b
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2024-02-18 18:01:54 +0100

    testing CI policy

--- Organiser.Core/Organiser.Core/Settings/EmailSenderSettings.cs
+++ Organiser.Core/Organiser.Core/Settings/EmailSenderSettings.cs
@@ -2,7 +2,7 @@
 {
     public class EmailSenderSettings
     {
-        public string? Host { get; set; }
+        public string? Host 
         public int Port { get; set; }
         public string? Login { get; set; }
         public string? Password { get; set; }
