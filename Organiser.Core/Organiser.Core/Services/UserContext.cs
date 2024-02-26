dfe46ea6e511b7a42395be5ee7a50e5ddbc2bcd2
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2024-02-26 14:30:08 +0100

    refactor

--- Organiser.Core/Organiser.Core/Services/UserContext.cs
+++ Organiser.Core/Organiser.Core/Services/UserContext.cs
@@ -5,10 +5,7 @@ namespace Organiser.Cores.Services
     public class UserContext : IUserContext
     {
         private readonly IHttpContextAccessor httpContextAccessor;
-        public UserContext(IHttpContextAccessor httpContextAccessor) 
-        { 
-            this.httpContextAccessor = httpContextAccessor;
-        }
+        public UserContext(IHttpContextAccessor httpContextAccessor) => this.httpContextAccessor = httpContextAccessor;
 
         public ClaimsPrincipal? User => httpContextAccessor.HttpContext?.User;
         public int UID => User == null ? 1 : int.Parse(User?.FindFirst(x => x.Type == "UID")?.Value);
