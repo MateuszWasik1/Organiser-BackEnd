c0310ee9f88c809e0addf002c5703b4692788666
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2024-03-03 15:18:16 +0100

    UnitTests for Accounts

---
+++ Organiser.Core/Organiser.UnitTests/CQRS/QueryHandler/Accounts/TestLoginQueryHandler.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Moq;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using Organiser.Core.CQRS.Resources.Accounts.Handlers;
+using Organiser.Core.CQRS.Resources.Accounts.Queries;
+using Organiser.Cores;
+using Organiser.Cores.Context;
+using Organiser.Cores.Entities;
+
+namespace Organiser.UnitTests.CQRS.QueryHandler.Accounts
+{
+    [TestFixture]
+    public class TestLoginQueryHandler
+    {
+        private Mock<IDataBaseContext> context;
+        private Mock<IPasswordHasher<User>> hasher;
+        private AuthenticationSettings authenticationSettings;
+
+        private List<User> users;
+
+        [SetUp]
+        public void SetUp()
+        {
+            context = new Mock<IDataBaseContext>();
+            hasher = new Mock<IPasswordHasher<User>>();
+            authenticationSettings = new AuthenticationSettings();
+
+            users = new List<User>()
+            {
+                new User()
+                {
+                    UID = 1,
+                    UGID = new Guid("30dd879c-ee2f-11db-8314-0800200c9a66"),
+                    URID = 1,
+                    UFirstName = "UFirstName1",
+                    ULastName = "ULastName1",
+                    UUserName = "Test1",
+                    UPassword = "Password1",
+                },
+            };
+
+            authenticationSettings = new AuthenticationSettings()
+            {
+                JWTKey = "JWT_KEY_FOR_LOGIN_QUERY_HANDLER_METHOD_UNIT_TESTS",
+                JWTIssuer = "JWTIssuer",
+                JWTExpiredDays = 1,
+            };
+
+            context.Setup(x => x.AllUsers).Returns(users.AsQueryable());
+
+            hasher.Setup(x => x.VerifyHashedPassword(It.IsAny<User>(), It.IsAny<string>(), It.IsAny<string>())).Returns(PasswordVerificationResult.Failed);
+
+        }
+
+        [Test]
+        public void TestLoginQueryHandler_NoUserNameFound_ShouldThrowException()
+        {
+            //Arrange 
+            var query = new LoginQuery() { Username = "RandomUser", Password = users[0].UPassword };
+            var handler = new LoginQueryHandler(context.Object, hasher.Object, authenticationSettings);
+
+            //Act
+            //Assert
+            Assert.Throws<Exception>(() => handler.Handle(query));
+        }
+
+        [Test]
+        public void TestLoginQueryHandler_UserNameFound_PasswordIncorrect_ShouldThrowException()
+        {
+            //Arrange 
+            var query = new LoginQuery() { Username = users[0].UUserName, Password = "RandomPassword" };
+            var handler = new LoginQueryHandler(context.Object, hasher.Object, authenticationSettings);
+
+            //Act
+            //Assert
+            Assert.Throws<Exception>(() => handler.Handle(query));
+        }
+
+        [Test]
+        public void TestLoginQueryHandler_UserNameAndPasswordCorrect_ShouldReturnJWTToken()
+        {
+            //Arrange 
+            hasher.Setup(x => x.VerifyHashedPassword(It.IsAny<User>(), It.IsAny<string>(), It.IsAny<string>())).Returns(PasswordVerificationResult.Success);
+
+            var query = new LoginQuery() { Username = users[0].UUserName, Password = users[0].UPassword };
+            var handler = new LoginQueryHandler(context.Object, hasher.Object, authenticationSettings);
+
+            //Act
+            var result = handler.Handle(query);
+
+            //Assert
+            ClassicAssert.IsInstanceOf<string>(result);
+        }
+    }
+}
---
+++ Organiser.Core/Organiser.UnitTests/Controllers/TestAccountsController.cs
@@ -0,0 +1,47 @@
+﻿using Moq;
+using NUnit.Framework;
+using Organiser.Core.CQRS.Dispatcher;
+using Organiser.Core.CQRS.Resources.Accounts.Commands;
+using Organiser.Core.CQRS.Resources.Accounts.Queries;
+using Organiser.Cores.Controllers;
+using Organiser.Cores.Models.ViewModels.AccountsViewModel;
+
+namespace Organiser.UnitTests.Controllers
+{
+    [TestFixture]
+    public class TestAccountsController
+    {
+        private Mock<IDispatcher> dispatcher;
+
+        [SetUp]
+        public void SetUp() => dispatcher = new Mock<IDispatcher>();
+
+        [Test]
+        public void TestAccountsController_Register_ShouldDispatch_RegisterUserCommand()
+        {
+            //Arrange
+            var controller = new AccountsController(dispatcher.Object);
+
+            //Act
+            controller.Register(new RegisterViewModel());
+
+            //Assert
+
+            dispatcher.Verify(x => x.DispatchCommand(It.IsAny<RegisterUserCommand>()), Times.Once);
+        }
+
+        [Test]
+        public void TestAccountsController_Login_ShouldDispatch_LoginQuery()
+        {
+            //Arrange
+            var controller = new AccountsController(dispatcher.Object);
+
+            //Act
+            controller.Login("", "");
+
+            //Assert
+
+            dispatcher.Verify(x => x.DispatchQuery<LoginQuery, string>(It.IsAny<LoginQuery>()), Times.Once);
+        }
+    }
+}
