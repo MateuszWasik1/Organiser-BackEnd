ae19ea09e50b8deb9be8cd0b129d5940316cc2ce
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2024-03-13 15:10:14 +0100

    Controller

---
+++ Organiser.Core/Organiser.Core/Controllers/NotesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Organiser.Core.CQRS.Dispatcher;
+
+namespace Organiser.Cores.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        private readonly IDispatcher dispatcher;
+        public NotesController(IDispatcher dispatcher) => this.dispatcher = dispatcher;
+
+        [HttpGet]
+        [Route("GetNotes")]
+        public string GetNotes()
+            => dispatcher.DispatchQuery<GetNotesQuery, string>(new GetNotesQuery());
+
+        [HttpPost]
+        [Route("AddNote")]
+        public void AddNote(NoteViewModel model)
+            => dispatcher.DispatchCommand(new AddNoteCommand() { Model = model });
+        
+        [HttpPut]
+        [Route("UpdateNote")]
+        public void UpdateNote(NoteViewModel model)
+            => dispatcher.DispatchCommand(new UpdateNoteCommand() { Model = model });
+
+        [HttpPut]
+        [Route("DeleteNote")]
+        public void DeleteNote(Guid ngid)
+            => dispatcher.DispatchCommand(new DeleteCommand() { Model = model });
+    }
+}
