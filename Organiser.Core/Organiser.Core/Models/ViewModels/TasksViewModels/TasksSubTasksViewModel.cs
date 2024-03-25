a7797b20f026b4324bbb8209cdda24b3c8aa1d37
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2024-03-25 17:13:34 +0100

    ViewModels

---
+++ Organiser.Core/Organiser.Core/Models/ViewModels/TasksViewModels/TasksSubTasksViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Organiser.Cores.Models.Enums;
+
+namespace Organiser.Core.Models.ViewModels.TasksViewModels
+{
+    public class TasksSubTasksViewModel
+    {
+        public Guid TSTGID { get; set; }
+        public string? TSTTitle { get; set; }
+        public string? TSTNext { get; set; }
+        public SubTasksStatusEnum TSTStatus { get; set; }
+    }
+}
