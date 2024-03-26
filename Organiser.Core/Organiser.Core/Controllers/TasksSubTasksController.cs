635e1396a4b651cec97b582c3885475870eece57
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2024-03-26 21:59:45 +0100

    dev

---
+++ Organiser.Core/Organiser.Core/Controllers/TasksSubTasksController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Organiser.Core.CQRS.Dispatcher;
+using Organiser.Core.CQRS.Resources.Tasks.TasksNotes.Commands;
+using Organiser.Core.CQRS.Resources.Tasks.TasksNotes.Queries;
+using Organiser.Core.Models.ViewModels.TasksViewModels;
+
+namespace Organiser.Cores.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class TasksSubTasksController : ControllerBase
+    {
+        private readonly IDispatcher dispatcher;
+        public TasksSubTasksController(IDispatcher dispatcher) => this.dispatcher = dispatcher;
+
+        //[HttpGet]
+        //public List<TasksNotesViewModel> Get(Guid tGID)
+        //    => dispatcher.DispatchQuery<GetTaskNoteQuery, List<TasksNotesViewModel>>(new GetTaskNoteQuery() { TGID = tGID });
+
+        //[HttpPost]
+        //[Route("AddTaskNote")]
+        //public void AddTaskNote(TasksNotesAddViewModel model)
+        //    => dispatcher.DispatchCommand(new AddTaskNoteCommand() { Model = model });
+
+        //[HttpDelete]
+        //[Route("DeleteTaskNote/{tNGID}")]
+        //public void DeleteTaskNote(Guid tNGID)
+        //    => dispatcher.DispatchCommand(new DeleteTaskNoteCommand() { TNGID = tNGID });
+    }
+}
