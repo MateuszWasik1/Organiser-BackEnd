85f3b74c86205b5bb8678f161e2862b510f14769
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2024-03-31 17:08:28 +0200

    Delete SubTask

---
+++ Organiser.Core/Organiser.Core/CQRS/Resources/Tasks/TasksSubTasks/Commands/DeleteTaskSubTaskCommand.cs
@@ -0,0 +1,9 @@
+﻿using Organiser.CQRS.Abstraction.Commands;
+
+namespace Organiser.Core.CQRS.Resources.Tasks.TasksSubTasks.Commands
+{
+    public class DeleteTaskSubTaskCommand : ICommand
+    {
+        public Guid TSTGID { get; set; }
+    }
+}
---
+++ Organiser.Core/Organiser.Core/CQRS/Resources/Tasks/TasksSubTasks/Handlers/DeleteTaskSubTaskCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using Organiser.Core.CQRS.Resources.Tasks.TasksSubTasks.Commands;
+using Organiser.Core.Exceptions.Tasks;
+using Organiser.Cores.Context;
+using Organiser.CQRS.Abstraction.Commands;
+
+namespace Organiser.Core.CQRS.Resources.Tasks.Tasks.Handlers
+{
+    public class DeleteTaskSubTaskCommandHandler : ICommandHandler<DeleteTaskSubTaskCommand>
+    {
+        private readonly IDataBaseContext context;
+        public DeleteTaskSubTaskCommandHandler(IDataBaseContext context) => this.context = context;
+
+        public void Handle(DeleteTaskSubTaskCommand command)
+        {
+            var subtask = context.TasksSubTasks.FirstOrDefault(x => x.TSTGID == command.TSTGID);
+
+            if (subtask == null)
+                throw new TasksSubTasksNotFoundExceptions("Nie znaleziono podzadania!");
+
+            context.DeleteTask(subtask);
+            context.SaveChanges();
+        }
+    }
+}
