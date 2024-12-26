9a9d8b6f8cb817ae064aae64545b8ac973c7a128
Mateusz Wąsik <mateuszwasikkontakt@gmail.com>
2024-12-26 18:36:26 +0100

    Bugs .AsNoTracking()

--- Organiser.Core/Organiser.Core/CQRS/Resources/Bugs/Bugs/Handlers/GetBugQueryHandle.cs
+++ Organiser.Core/Organiser.Core/CQRS/Resources/Bugs/Bugs/Handlers/GetBugQueryHandle.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using Organiser.Core.CQRS.Resources.Bugs.Bugs.Queries;
 using Organiser.Core.Exceptions.Bugs;
 using Organiser.Core.Models.ViewModels.BugsViewModels;
@@ -24,12 +25,12 @@ namespace Organiser.Core.CQRS.Resources.Bugs.Bugs.Handlers
         public BugViewModel Handle(GetBugQuery query)
         {
             var bug = new Cores.Entities.Bugs();
-            var currentUserRole = context.User.FirstOrDefault(x => x.UID == user.UID)?.URID ?? (int) RoleEnum.User;
+            var currentUserRole = context.User.AsNoTracking().FirstOrDefault(x => x.UID == user.UID)?.URID ?? (int) RoleEnum.User;
 
             if (currentUserRole == (int) RoleEnum.Admin || currentUserRole == (int) RoleEnum.Support)
-                bug = context.AllBugs.FirstOrDefault(x => x.BGID == query.BGID);
+                bug = context.AllBugs.AsNoTracking().FirstOrDefault(x => x.BGID == query.BGID);
             else
-                bug = context.Bugs.FirstOrDefault(x => x.BGID == query.BGID);
+                bug = context.Bugs.AsNoTracking().FirstOrDefault(x => x.BGID == query.BGID);
 
             if (bug == null)
                 throw new BugNotFoundExceptions("Nie znaleziono wskazanego błędu!");
